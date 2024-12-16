9bd8b5ec725f2221a11198f92539f9ed308cf186
collinrsmith <89951648+collinrsmith@users.noreply.github.com>
2024-12-15 20:18:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ const port = process.env.PORT || 3001;
 const bodyParser = require("body-parser"); 
 require("dotenv").config({ path: path.resolve(__dirname, 'credentials/.env') })
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const user = process.env.MONGO_DB_USERNAME;
+const pass = process.env.MONGO_DB_PASSWORD;
+const uri = `mongodb+srv://${user}:${pass}@cluster0.5x6o3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const databaseAndCollection = {db: process.env.MONGO_DB_NAME,
+                               collection: process.env.MONGO_COLLECTION
+                              };
 
-app.set("views", path.resolve(__dirname, "templates"));
+app.set("views", path.resolve(__dirname, "/"));
 app.set("view engine", "ejs");
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.render('index');
+app.use(bodyParser.urlencoded({extended:false}));
+
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
