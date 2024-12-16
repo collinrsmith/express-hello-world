199ad6e26cbd26aab0b6dad9d50c241500c98b78
collinrsmith <89951648+collinrsmith@users.noreply.github.com>
2024-12-15 20:27:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 const bodyParser = require("body-parser"); 
-require("dotenv").config({ path: path.resolve(__dirname, 'credentials/.env') })
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const user = process.env.MONGO_DB_USERNAME;
-const pass = process.env.MONGO_DB_PASSWORD;
+const reader = new FileReader();
+const user = reader.readAsText("etc/secrets/MONGO_DB_USERNAME");
+const pass = reader.readAsText("etc/secrets/MONGO_DB_PASSWORD");
 const uri = `mongodb+srv://${user}:${pass}@cluster0.5x6o3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const databaseAndCollection = {db: process.env.MONGO_DB_NAME,
-                               collection: process.env.MONGO_COLLECTION
+const databaseAndCollection = {db: reader.readAsText("etc/secrets/MONGO_DB_NAME"),
+                               collection: reader.readAsText("etc/secrets/MONGO_COLLECTION")
                               };
 
 app.set("views", path.resolve(__dirname, "/"));
